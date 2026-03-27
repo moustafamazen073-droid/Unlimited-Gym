431694035cb1e40b3009a091ee894a49c9170200
moustafamazen073-droid <moustafamazen073@gmail.com>
2026-03-27 21:53:37 +0200

    Update gym.js

--- gym.js
+++ gym.js
@@ -747,3 +747,19 @@ function handleInitialHash() {
 
 // شغلها عند تحميل الصفحة
 window.addEventListener('load', handleInitialHash);
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HPHG3W3XD9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HPHG3W3XD9');
+</script>
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "w2fnvxst5d");
+</script>
