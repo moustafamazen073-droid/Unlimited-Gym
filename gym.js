fc8bb9c4ee93bb4da5bf0191a846af06f7a58d84
moustafamazen073-droid <moustafamazen073@gmail.com>
2026-03-28 02:59:46 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- gym.js
+++ gym.js
@@ -747,3 +747,23 @@ function handleInitialHash() {
 
 // شغلها عند تحميل الصفحة
 window.addEventListener('load', handleInitialHash);
+// Google Analytics
+(function(){
+  var script = document.createElement("script");
+  script.async = true;
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-HPHG3W3XD9";
+  document.head.appendChild(script);
+})();
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-HPHG3W3XD9');
+
+
+// Microsoft Clarity
+(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "w2fnvxst5d");
